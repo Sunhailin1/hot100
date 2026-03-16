12a03f41eee45547cfff826aa51e1a8eae5373bc
BestHl <497724416@qq.com>
2026-03-16 21:36:00 +0800

    测试合并

--- src/main/java/com/offermg/shl/Main.java
+++ src/main/java/com/offermg/shl/Main.java
@@ -7,5 +7,8 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Keep Going!");
+
+        // master
+        System.out.println("Master!");
     }
 }
