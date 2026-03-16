520c2694097dcda289498fa5df092a34c36d09dc
BestHl <497724416@qq.com>
2026-03-16 20:48:56 +0800

    3_16完成

--- src/main/java/com/offermg/shl/sliceWindow/lc_3.java
+++ src/main/java/com/offermg/shl/sliceWindow/lc_3.java
@@ -1,5 +1,25 @@
 package com.offermg.shl.sliceWindow;
 
-public class lc_3 {
+import java.util.HashSet;
+import java.util.Set;
 
+public class lc_3 {
+    public int lengthOfLongestSubstring(String s) {
+        int maxLength = 0;
+        int left = 0;
+        int right = 0;
+        Set<Character> set = new HashSet<>();
+
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            while (set.contains(c)) {
+                set.remove(s.charAt(left++));
+            }
+            right++;
+            set.add(c);
+            maxLength = Math.max(right - left, maxLength);
+        }
+
+        return maxLength;
+    }
 }
